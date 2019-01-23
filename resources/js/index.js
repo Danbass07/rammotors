1b92c31423e8d71111fbe2df6d8dcdbda08d0c49
Danbass07 <danbass666@googlemail.com>
2019-01-23 11:51:45 +0000

    first commit

---
+++ resources/js/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import RamMotors from './components/RamMotors';
+
+
+if (document.getElementById('app')) {
+    ReactDOM.render(
+    
+    <RamMotors />
+    
+    , document.getElementById('app'));
+}
