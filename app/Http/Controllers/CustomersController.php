f22c201993111258fb55ddd102623ddcce957874
Danbass07 <danbass666@googlemail.com>
2019-04-09 11:29:45 +0100

    AUTH UPDATE

--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -9,6 +9,10 @@ use Illuminate\Support\Facades\Log;
 
 class CustomersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     /**
      * Display a listing of the resource.
      *
